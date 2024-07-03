350399374baf87025e985186d2938adc086c0199
Amit Namdeo <amit.namdeo@oracle.com>
2024-07-03 13:16:01 -0500

    update terraform required version to >=1.2.9

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  #required_version = ">= 1.3.0"
+  required_version = ">=1.2.9"
   required_providers {
     # https://registry.terraform.io/providers/hashicorp/azuread/latest/docs
     azuread = {
